dcee7e31cbc1aa2d778eb56b71a8aa525de1c6c1
Yash <Yash250619@gmail.com>
2021-12-05 11:59:14 -0500

    again modified

--- src/test/java/com/example/TestGreeter.java
+++ src/test/java/com/example/TestGreeter.java
@@ -31,7 +31,7 @@ public class TestGreeter {
   public void greetShouldIncludeGreetingPhrase() {
     String someone = "World";
 
-    assertThat(greeter.greet(someone).length(), is(greaterThan(someone.length())));
+    assertThat(greeter.greet(someone).length(), is(lessThan(someone.length())));
   }
 
     @Test
