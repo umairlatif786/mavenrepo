7819350aded72a91d3119398b63640862884c45f
root <root@ip-172-31-8-143.ca-central-1.compute.internal>
2022-03-06 16:49:38 +0000

    code updated

--- src/main/java/com/example/Greeter.java
+++ src/main/java/com/example/Greeter.java
@@ -21,6 +21,6 @@ public class Greeter {
   
   public static void main(String args[]){
     
-	  System.out.println("Hello from DevOps");
+	  System.out.println("Hello from DevOps by umair latif");
   }
 }
